dd352f35ad5bf56b6214d4e3e1b0e07fe337e8c2
Sean Campbell <sean.campbell13@gmail.com>
2025-02-21 18:02:27 -0500

    build an appliation ch 1

---
+++ build-an-application/in_memory_player_store.go
@@ -0,0 +1,25 @@
+package main
+
+import "sync"
+
+// in_memory_player_store.go
+func NewInMemoryPlayerStore() *InMemoryPlayerStore {
+	return &InMemoryPlayerStore{map[string]int{}, sync.RWMutex{}}
+}
+
+type InMemoryPlayerStore struct {
+	store map[string]int
+	mu    sync.RWMutex
+}
+
+func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+	i.store[name]++
+}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	i.mu.RLock()
+	defer i.mu.RUnlock()
+	return i.store[name]
+}
